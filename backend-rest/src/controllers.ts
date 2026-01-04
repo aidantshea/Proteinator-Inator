2ef9429f7871916ac0ba4351953391676952a039
Aidan Shea <aidantshea@gmail.com>
2026-01-04 12:46:47 -0800

    feat: hello typescript
    Initializes development environment for TypeScript programming,
    including development and build scripts in package.json. A single
    route handler implements a callback function which sends a JSON
    response with the text 'Hello, TypeScript!' back to the client.

---
+++ backend-rest/src/controllers.ts
@@ -0,0 +1,14 @@
+import express, { type Request, type Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+    res.json({ status: 'Hello, TypeScript!' });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}...`);
+})
